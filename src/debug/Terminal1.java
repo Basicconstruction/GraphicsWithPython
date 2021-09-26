1a3b86c841f4a60a8487c6506ddad158babd2e59
wcx <bethaniebell8474@gmail.com>
2021-09-26 23:02:58 +0800

    更新 java执行java代码

--- src/debug/Terminal1.java
+++ src/debug/Terminal1.java
@@ -1,20 +1,51 @@
 package debug;
 
-import datacollector.EasySplit;
-import datacollector.SpaceFilter;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Terminal1 {
+    static int a = 0;
     public static void main(String[] args) throws IOException {
-        System.out.println(SpaceFilter.getFilterText("12     34"));
-        String s = "12  34";
-        for(String d:new EasySplit(" ").getSplits(s)){
-            System.out.println(d);
+    Thread thread1 =
+        new Thread(
+            new Runnable() {
+
+              @Override
+              public void run() {
+                try {
+                  Process process =
+                      Runtime.getRuntime()
+                          .exec(
+                              new String[] {"cmd", "/c", "javac Avb.java"},
+                              null,
+                              new File("D:\\GraphicsWithPython2\\"));
+                  a = 1;
+                } catch (IOException e) {
+                  e.printStackTrace();
+                }
+              }
+            });
+    thread1.start();
+    Thread thread2 = new Thread(()->{
+        try {
+            while(a!=1){
+                Thread.sleep(20);
+            }
+            Process process =
+                    Runtime.getRuntime()
+                            .exec(new String[] {"cmd", "/c","java Avb"}, null, new File("D:\\GraphicsWithPython2\\"));
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
+    });
+    thread2.start();
+//        System.out.println(SpaceFilter.getFilterText("12     34"));
+//        String s = "12  34";
+//        for(String d:new EasySplit(" ").getSplits(s)){
+//            System.out.println(d);
+//        }
 //        Process process = Runtime.getRuntime()
 //                .exec("cmd /c dir", null, new File("D://"));
 //        printResults(process);
